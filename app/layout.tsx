aa6af749e3f03edb7494c44c56af19ad00843f01
Tomoe-12 <li8993han@gmail.com>
2025-05-28 23:47:28 +0630

    Update layout metadata and improve Open Graph settings for portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,27 @@
 import type { Metadata } from 'next'
 import './globals.css'
-
+import {Profile_image} from '@/data/personal'
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Khun Thi Han | Full Stack Developer',
+  description: 'Welcome to the personal portfolio of Khun Thi Han...',
+  openGraph: {
+    title: 'Khun Thi Han | Full Stack Developer',
+    description: 'Explore projects, skills, and contact information of Khun Thi Han...',
+    url: 'https://khunthihan.vercel.app/', // replace with your actual domain
+    siteName: 'Khun Thi Han Portfolio',
+    images: [
+      {
+        url: Profile_image,
+        width: 1200,
+        height: 630,
+        alt: 'Khun Thi Han Portfolio',
+      },
+    ],
+    type: 'website',
+  },
 }
+
+
 
 export default function RootLayout({
   children,
@@ -14,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <link rel="icon" href={Profile_image} type='image/x-icon' />
+    </head>
       <body>{children}</body>
     </html>
   )
