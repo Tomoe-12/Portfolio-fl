dc763f5599060c002655a10cf33cdf65ca6f3a36
Tomoe-12 <li8993han@gmail.com>
2025-10-06 19:53:56 +0630

    Update resume link to new URL and comment out the old link

--- data/personal.ts
+++ data/personal.ts
@@ -1,7 +1,8 @@
 export const Burmese_name = "ခွန်သီဟန်";
 export const English_name = "Khun Thi Han";
 export const Resume_link =
-  "https://eib21y5pae.ufs.sh/f/2vuqpytl1cW9KoTwTBiNP6Qqa83Slmv9gZduTA2zKLfoEWY7";
+  // "https://eib21y5pae.ufs.sh/f/2vuqpytl1cW9KoTwTBiNP6Qqa83Slmv9gZduTA2zKLfoEWY7",
+  "https://eib21y5pae.ufs.sh/f/2vuqpytl1cW9naDnFM7dugfkliNaQH9BtqD7MALTvF6Z31Ky";
 export const Profile_image =
   "https://eib21y5pae.ufs.sh/f/2vuqpytl1cW9iaEmF8vXw1t5WKq6aml3Yo8QU7pZri2nMkCO";
 
